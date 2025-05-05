d1f13a4b0693f4a8ab8d080e3ea11a833b951813
ANUJ KUMAR <anuj1994k>
2025-05-05 19:53:06 +0530

    controller error

--- construction-backend/routes/siteRoutes.js
+++ construction-backend/routes/siteRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const siteController=require("../controllers/SiteController.js")
+const siteController=require("../controllers/SiteController")
 
 router.post("/", siteController.createSite);
 router.get("/", siteController.getAllSites);
